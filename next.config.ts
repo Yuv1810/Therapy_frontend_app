16cc6279241f80db1030b47f5290d188e9856722
Yuvraj Singh <engyuvrajsingh@gmail.com>
2026-09-06 22:35:32 +0530

    image change2

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: [
     '39a6-2405-201-8003-582b-f899-7709-52e3-83ca.ngrok-free.app',
   ],
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://34.131.31.125:5500/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
